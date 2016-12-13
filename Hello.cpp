094953b3985c8cacdb602cd9005d58cbc6a9dd10
kingoflegend95 <vulinh16111995@gmail.com>
2016-12-13 22:29:37 +0700

    First lesson
    Bài tập hello

---
+++ Hello.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	
+	printf("Chao cac ban");
+	
+
+    return 0;
+}
+
---
+++ conghoaxahoichunghiavn.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	printf("Chao cac ban\n");
+	printf("Cong Hoa Xa Hoi Chu Nghia Viet Nam\n");
+	printf("Doc lap* Tu do* Hanh phuc\n");
+
+    return 0;
+}
+
